5f4a8b67d304d5e9f09ed72f9879294354ef6ad4
Robert Buhren <robert@robertbuhren.de>
2020-06-28 20:38:34 +0200

    [psp-misc]: Cleanup/refactor PSP MISC

--- hw/arm/psp-misc.c
+++ hw/arm/psp-misc.c
@@ -15,6 +15,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Special PSP device that covers various unknown MMIO regs.
  */
 #include "qemu/osdep.h"
 #include "qapi/error.h"
@@ -36,75 +38,60 @@
  * TODO: Implement call back to handle more complicated misc devices
  */
 
-static void psp_misc_set_identifier(Object *obj, const char *str, Error **errp) {
+static void psp_misc_set_identifier(Object *obj, const char *str, Error **errp)
+{
     PSPMiscState *s = PSP_MISC(obj);
 
     s->ident = g_strdup(str);
 }
 
-static char *psp_misc_get_identifier(Object *obj, Error **errp) {
+static char *psp_misc_get_identifier(Object *obj, Error **errp)
+{
     PSPMiscState *s = PSP_MISC(obj);
 
     return g_strdup(s->ident);
 }
 
-static const char* get_region_name(hwaddr addr) {
-
-    /* TODO Fixme */
-
-    if (addr > PSP_SMN_BASE && addr < PSP_MMIO_BASE) {
-        return PSP_SMN_NAME;
-    } else if (addr > PSP_MMIO_BASE && addr < PSP_X86_BASE) {
-        return PSP_MMIO_NAME;
-    } else if (addr > PSP_X86_BASE && addr < PSP_UNKNOWN_BASE) {
-        return PSP_X86_NAME;
-    } else if (addr > PSP_UNKNOWN_BASE && addr < PSP_ROM_BASE) {
-        return PSP_UNKNOWN_NAME;
-    }
-
-    return PSP_ROM_NAME;
-}
-
-static void psp_misc_write(void *opaque, hwaddr offset,
-                       uint64_t value, unsigned int size) {
+static void psp_misc_write(void *opaque, hwaddr offset, uint64_t value,
+                           unsigned int size)
+{
     PSPMiscState *misc = PSP_MISC(opaque);
     hwaddr phys_base = misc->iomem.addr;
+    hwaddr phys_abs;
 
     /* TODO: Allow write to specific registers */
 
-    /* We expect the pre-seeded memory regions to be instantiated with
-     * physical addresses. Hence the compare with "phys_base + offset".
-     */
-    offset += phys_base;
+    phys_abs = phys_base + offset;
 
-    qemu_log_mask(LOG_UNIMP, "%s: unimplemented device write at %s "
-                  "(size %d, offset 0x%" HWADDR_PRIx ", val 0x%lx)\n",
-                  misc->ident, get_region_name(offset),size, offset, value);
+    qemu_log_mask(LOG_UNIMP, "%s: unimplemented device write at 0x%"
+                  HWADDR_PRIx " (size %d, offset 0x%" HWADDR_PRIx ", value " \
+                  "0x%lx)\n", misc->ident, phys_abs, size, offset, value);
 }
 
-static uint64_t psp_misc_read(void *opaque, hwaddr offset, unsigned int size) {
+static uint64_t psp_misc_read(void *opaque, hwaddr offset, unsigned int size)
+{
     
-    /* TODO: Get current PC and regs */
     PSPMiscState *misc = PSP_MISC(opaque);
-    
-    hwaddr phys_base = misc->iomem.addr;
+    hwaddr phys_abs;
     int i;
+    hwaddr phys_base;
+    uint64_t value;
 
-    /* We expect the pre-seeded memory regions to be instantiated with
-     * physical addresses. Hence the compare with "phys_base + offset".
-     */
-    offset += phys_base;
-
+    phys_base = misc->iomem.addr;
+    phys_abs = phys_base + offset;
+    value = 0;
 
     for (i = 0; i < misc->regs_count; i++) {
-        if (offset == misc->regs[i].addr)
-            return misc->regs[i].val;
+        if (phys_abs == misc->regs[i].addr)
+            value = misc->regs[i].val;
     }
 
-    qemu_log_mask(LOG_UNIMP, "%s: unimplemented device read at %s "
-                  "(size %d, offset 0x%" HWADDR_PRIx ")\n",
-                  misc->ident, get_region_name(offset),size, offset);
-    return 0;
+    phys_abs = phys_base + offset;
+
+    qemu_log_mask(LOG_UNIMP, "%s: unimplemented device read at 0x%"
+                  HWADDR_PRIx " (size %d, offset 0x%" HWADDR_PRIx ", value " \
+                  "0x%lx)\n", misc->ident, phys_abs, size, offset, value);
+    return value;
 }
 
 static const MemoryRegionOps misc_mem_ops = {
@@ -118,6 +105,9 @@ static const MemoryRegionOps misc_mem_ops = {
 static void psp_misc_init(Object *obj)
 {
     PSPMiscState *s = PSP_MISC(obj);
+
+    s->mmio_size = 0;
+    s->ident = NULL;
     
     /* TODO: What is the difference to e.g. DEFINE_PROP_UINT32 ? */
     object_property_add_uint64_ptr(obj,"psp_misc_msize", &s->mmio_size,
@@ -131,19 +121,28 @@ static void psp_misc_init(Object *obj)
 static void psp_misc_realize(DeviceState *dev, Error **errp)
 {
     PSPMiscState *s = PSP_MISC(dev);
-    SysBusDevice *sbd = SYS_BUS_DEVICE(dev);
+
+    if (s->mmio_size == 0) {
+        error_setg(errp, "property 'mmio_size' not specified or zero");
+        return;
+    }
+
+    if (s->ident == NULL) {
+        error_setg(errp, "property 'ident' not specified");
+        return;
+    }
 
     memory_region_init_io(&s->iomem, OBJECT(dev), &misc_mem_ops, s,
-            TYPE_PSP_MISC, s->mmio_size);
+                          s->ident, s->mmio_size);
 
-    sysbus_init_mmio(sbd, &s->iomem);
+    sysbus_init_mmio(SYS_BUS_DEVICE(s), &s->iomem);
 
 }
 
-static void psp_misc_class_init(ObjectClass *oc, void *data) {
+static void psp_misc_class_init(ObjectClass *oc, void *data)
+{
     DeviceClass *dc = DEVICE_CLASS(oc);
     dc->realize = psp_misc_realize;
-
 }
 
 static const TypeInfo psp_misc_info = {
@@ -155,8 +154,9 @@ static const TypeInfo psp_misc_info = {
 
 };
 
-static void psp_misc_register_types(void) {
+static void psp_misc_register_types(void)
+{
     type_register_static(&psp_misc_info);
-
 }
+
 type_init(psp_misc_register_types);
